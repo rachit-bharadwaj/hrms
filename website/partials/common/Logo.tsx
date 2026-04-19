2e2793b6bcee0936899beb02232c81d05c71a886
Rachit Bharadwaj <rachitbharadwaj52@gmail.com>
2026-04-19 23:49:50 +0530

    refactor: self-close div elements in Logo component for cleaner JSX syntax

--- website/partials/common/Logo.tsx
+++ website/partials/common/Logo.tsx
@@ -6,10 +6,10 @@ export default function Logo({ size = 10 }: { size?: number }) {
       >
         {/* Abstract geometric logo inspired by the screenshot */}
         <div className="grid grid-cols-2 gap-1 w-full h-full">
-          <div className="bg-blue-600 rounded-lg rounded-br-none"></div>
-          <div className="bg-cyan-400 rounded-lg rounded-bl-none"></div>
-          <div className="bg-blue-400 rounded-lg rounded-tr-none"></div>
-          <div className="bg-blue-700 rounded-lg rounded-tl-none"></div>
+          <div className="bg-blue-600 rounded-lg rounded-br-none" />
+          <div className="bg-cyan-400 rounded-lg rounded-bl-none" />
+          <div className="bg-blue-400 rounded-lg rounded-tr-none" />
+          <div className="bg-blue-700 rounded-lg rounded-tl-none" />
         </div>
       </div>
     </div>
